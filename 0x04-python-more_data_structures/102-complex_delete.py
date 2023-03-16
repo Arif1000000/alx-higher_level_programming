b781768ca5082c6ae1675b1e40cff0af7849adbb
issah arif <muhammed.arif11@gmail.com>
2023-03-15 18:25:32 -0700

    updated task

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def complex_dictionary, value):
+def complex_delete(a_dictionary, value):
     for i in list(a_dictionary.keys()):
         if a_dictionary[i] == value:
             a_dictionary.pop(i, None)
